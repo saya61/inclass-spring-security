f2647d2edda214765f35fdd1c38d88773513da6a
saya61 <msaki061@gmail.com>
2024-06-21 10:17:26 +0900

    MTM 관련 코드 주석 작성

--- src/main/java/ac/su/inclassspringsecurity/domain/Order.java
+++ src/main/java/ac/su/inclassspringsecurity/domain/Order.java
@@ -4,7 +4,9 @@ import ac.su.inclassspringsecurity.constant.OrderStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.FieldNameConstants;
+
+import java.util.List;
+
 
 @Entity
 @Getter @Setter
@@ -30,6 +32,14 @@ public class Order {
 
     @Column(nullable = false)
     private String updatedAt;
+
+    // 인라인 중계 테이블 생성(별도 엔티티를 사용하지 않음)
+//    @ManyToMany
+//    @JoinTable(
+//            name = "order_product",
+//            joinColumns = @JoinColumn(name = "order_id"),
+//            inverseJoinColumns = @JoinColumn(name = "product_id"))
+//    private List<Product> products;
 
     @Override
     public String toString() {
